94b978dabdfd204b2e8c664aa1fde06d5c1622f6
Goose <63527508+GooseBehindTheSlaughter@users.noreply.github.com>
2024-04-16 02:45:29 +0100

    Add files via upload

---
+++ NoSlowdown/Source/NoSlowdown/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+
+public class Program : Mod
+{
+    public Program(ModContentPack content) : base(content)
+    {
+            Harmony harmony = new Harmony("TheGooseBehindTheSlaughter.FuckForceSlowdown");
+            harmony.PatchAll();
+    }
+
+    //Verse.TimeSlower
+    [HarmonyPatch]
+    public static class SlowDown_Patch
+    {
+        [HarmonyPatch(typeof(TimeSlower), nameof(TimeSlower.SignalForceNormalSpeed))]
+        [HarmonyPrefix]
+        public static void SignalForceNormalSpeed(TimeSlower __instance)
+        {
+            DebugViewSettings.neverForceNormalSpeed = true;
+        }
+
+        [HarmonyPatch(typeof(TimeSlower), nameof(TimeSlower.SignalForceNormalSpeedShort))]
+        [HarmonyPrefix]
+        public static void SignalForceNormalSpeedShort(TimeSlower __instance)
+        {
+            DebugViewSettings.neverForceNormalSpeed = true;
+        }
+    }
+}
+
